09004f21ddbaf2b709b194c98ebe136a797df951
davidnam0514 <davidnam0514@gmail.com>
2022-06-10 06:24:25 -0500

    드림코딩 클래스퀴즈2 with 주석

--- quiz2.js
+++ quiz2.js
@@ -6,23 +6,23 @@
 
 
 class Employee{
-    constructor(name, department, totalHour, payRate){
+    constructor(name, department, totalHour, payRate){ // 직원들의 정보와, 페이레이트!! 이걸까먹었다.
         this.name = name;
         this.department = department;
         this.totalHour =totalHour;
-        this.payRate = payRate;
+        this.payRate = payRate;   // 그대로 찍어와주고
     }
 
     calculate = () => {
-       return this.totalHour * this.payRate; 
+       return this.totalHour * this.payRate; // 이것을위해 페이레이트가 필요했다. 
 
     }
 }
 
 class PartTimer extends Employee{
-    static PAY_RATE = 8000;
-    constructor(name, department, totalHour){
-        super(name, department, totalHour, PartTimer.PAY_RATE);
+    static PAY_RATE = 8000; // 매개변수에 숫자는 보기좋지않고, 코드를볼때 뭔지찾기 힘드므로, static변수를 이용하여 페이레이트를 할당한다.
+    constructor(name, department, totalHour){ // 직원들의 정보가 담긴건 3개고, 그밑에 슈퍼로 4개값을 상속해준다.
+        super(name, department, totalHour, PartTimer.PAY_RATE);//static변수는 클래스.스태틱변수 로 표기해준다. 왜인지는 공부해보자. 
     }
     }
 
@@ -35,8 +35,7 @@ class FullTimer extends Employee{
     }
 }
 
-// const jane = new Partimer('제인','server',100);
-// console.log(jane.calculate()) ;
+
 
 const jihye = new FullTimer('지혜', 'server', 30);
 const lera = new PartTimer('발레리아', 'kitchen', 40);
